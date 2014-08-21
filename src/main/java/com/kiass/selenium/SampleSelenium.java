f62b1de02a360c93ecced1c098cc9372499f2a18
phuthep <kiass@hotmail.com>
2014-08-21 23:14:11 +0700

    locators

--- src/main/java/com/kiass/selenium/SampleSelenium.java
+++ src/main/java/com/kiass/selenium/SampleSelenium.java
@@ -1,6 +1,7 @@
 package com.kiass.selenium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
@@ -14,11 +15,24 @@ public class SampleSelenium {
         //open selnium web page
         firefoxDriver.get("http://www.seleniumhq.org");
 
-        //find input text element and send text
-        firefoxDriver.findElement(By.id("q")).sendKeys("selenium");
+        //Find by css selector
+        firefoxDriver.findElement(By.cssSelector("#menu_projects a")).click();
 
-        //find submit button and click
-        firefoxDriver.findElement(By.id("submit")).click();
+        //Find by chaining element
+        firefoxDriver.findElement(By.id("mainContent")).findElement(By.className("bigMenu")).findElement(By.linkText("Selenium WebDriver")).click();
+
+        //Find by default return object
+        WebElement webelement = firefoxDriver.findElement(By.id("mainContent"));
+
+        //get text from element
+        String header = webelement.findElement(By.tagName("h2")).getText();
+
+        //get text from element
+        String subheader = webelement.findElement(By.tagName("i")).getText();
+
+        System.out.println("# SHOW HEADER " + header + " #");
+        System.out.println("# SHOW SUBHEADER " + subheader + " #");
+
 
         //close firefox driver
         firefoxDriver.quit();
